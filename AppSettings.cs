cbe3d3d5c7dfe721c351ed8d479eba7f2e592268
Mark Harrison <mark@harrison.ws>
2025-02-23 22:07:23 +0000

    Update Settings initialisation

---
+++ AppSettings.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+
+namespace SKProcess
+{
+    public class AppSettings
+    {
+        public string azopwnaiApikey { get; set; }
+        public string azopwnaiEndpoint { get; set; }
+        public string azopenaiCCDeploymentname { get; set; }
+        public string azopenaiEmbeddingDeploymentname { get; set; }
+        public bool traceOn { get; set; }
+        public string bingApikey { get; set; }
+        public string bingEndpoint { get; set; }
+        public string embeddingModelType { get; set; }
+        public string vectorStoreType { get; set; }
+        public string collectionName { get; set; }
+        public string ollamaEndpoint { get; set; }
+        public string ollamaEmbeddingModelId { get; set; }
+        public string bertEmbeddingModelPath { get; set; }
+        public string bertEmbeddingVocabPath { get; set; }
+        public string hfEndPoint { get; set; }
+        public string hfApikey { get; set; }
+        public string azaisearchEndpoint { get; set; }
+        public string azaisearchApikey { get; set; }
+        public string cosmosdbConnectionString { get; set; }
+        public string cosmosdbDatabaseName { get; set; }
+        public string docName { get; set; }
+        public string dirPath { get; set; }
+
+        public ConfigurationManager configuration;
+
+        public AppSettings()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "development";
+            var hostBuilder = Host.CreateApplicationBuilder();
+            hostBuilder.Configuration
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            configuration = hostBuilder.Configuration;
+
+            azopwnaiApikey = configuration["AzOpenAI:ApiKey"] ?? "";
+            azopwnaiEndpoint = configuration["AzOpenAI:Endpoint"] ?? "";
+            azopenaiCCDeploymentname = configuration["AzOpenAI:ChatCompletionDeploymentName"] ?? "";
+            azopenaiEmbeddingDeploymentname = configuration["AzOpenAI:EmbeddingDeploymentName"] ?? "";
+            ollamaEndpoint = configuration["Ollama:Endpoint"] ?? "";
+            ollamaEmbeddingModelId = configuration["Ollama:EmbeddingModelId"] ?? "";
+            bertEmbeddingModelPath = configuration["BertOnnx:EmbeddingModelPath"] ?? "";
+            bertEmbeddingVocabPath = configuration["BertOnnx:EmbeddingVocabPath"] ?? "";
+            hfEndPoint = configuration["HuggingFace:EndPoint"] ?? "";
+            hfApikey = configuration["HuggingFace:Apikey"] ?? "";
+            azaisearchEndpoint = configuration["AzAISearch:Endpoint"] ?? "";
+            azaisearchApikey = configuration["AzAISearch:ApiKey"] ?? "";
+            cosmosdbConnectionString = configuration["CosmosDB:ConnectionString"] ?? "";
+            cosmosdbDatabaseName = configuration["CosmosDB:DatabaseName"] ?? "";
+            traceOn = bool.TryParse(configuration["TraceOn"], out bool tOn) && tOn;
+            bingApikey = configuration["Bing:ApiKey"] ?? "";
+            bingEndpoint = configuration["Bing:Endpoint"] ?? "";
+            embeddingModelType = configuration["EmbeddingModelType"] ?? "";
+            collectionName = configuration["CollectionName"] ?? "";
+            vectorStoreType = configuration["VectorStoreType"] ?? "";
+            docName = configuration["Document:Name"] ?? "";
+            dirPath = configuration["Document:DirPath"] ?? "";
+            if (!dirPath.EndsWith("/")) dirPath += "/";
+
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
+using SKProcess;
 using System.Diagnostics.Contracts;
 
 #pragma warning disable SKEXP0001, SKEXP0003, SKEXP0003, SKEXP0011, SKEXP0020, SKEXP0050, SKEXP0052, SKEXP0055, SKEXP0011, SKEXP0010, SKEXP0070
@@ -17,52 +18,54 @@ namespace DocUploader
 
             Console.WriteLine("*** DocLoader ***");
 
-            var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "development";
-            var hostBuilder = Host.CreateApplicationBuilder(args);
-            hostBuilder.Configuration
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
-                .AddEnvironmentVariables();
+            AppSettings setx = new();
 
-            var configuration = hostBuilder.Configuration;
+            //var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "development";
+            //var hostBuilder = Host.CreateApplicationBuilder(args);
+            //hostBuilder.Configuration
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+            //    .AddEnvironmentVariables();
 
-            string docName = configuration["Document:Name"] ?? "";
-            string dirPath = configuration["Document:DirPath"] ?? "";
-            if (!dirPath.EndsWith("/")) dirPath += "/";
-            string vectorStoreType = configuration["VectorStoreType"] ?? "";
-            string embeddingModelType = configuration["EmbeddingModelType"] ?? "";
+            //var configuration = hostBuilder.Configuration;
+
+            //string docName = configuration["Document:Name"] ?? "";
+            //string dirPath = configuration["Document:DirPath"] ?? "";
+            //if (!dirPath.EndsWith("/")) dirPath += "/";
+            //string vectorStoreType = configuration["VectorStoreType"] ?? "";
+            //string embeddingModelType = configuration["EmbeddingModelType"] ?? "";
 
             var kernelBuilder = Kernel.CreateBuilder();
 
-            switch (embeddingModelType.ToLower())
+            switch (setx.embeddingModelType.ToLower())
             {
                 case "azopenai":
-                    string azopwnaiApikey = configuration["AzOpenAI:ApiKey"] ?? "";
-                    string azopwnaiEndpoint = configuration["AzOpenAI:Endpoint"] ?? "";
-                    string azopenaiCCDeploymentname = configuration["AzOpenAI:ChatCompletionDeploymentName"] ?? "";
-                    string azopenaiEmbeddingDeploymentname = configuration["AzOpenAI:EmbeddingDeploymentName"] ?? "";
-                    Console.WriteLine($"Using Azure OpenAI {azopenaiEmbeddingDeploymentname}");
-                    kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(azopenaiEmbeddingDeploymentname, azopwnaiEndpoint, azopwnaiApikey);
+                    //string azopwnaiApikey = configuration["AzOpenAI:ApiKey"] ?? "";
+                    //string azopwnaiEndpoint = configuration["AzOpenAI:Endpoint"] ?? "";
+                    //string azopenaiCCDeploymentname = configuration["AzOpenAI:ChatCompletionDeploymentName"] ?? "";
+                    //string azopenaiEmbeddingDeploymentname = configuration["AzOpenAI:EmbeddingDeploymentName"] ?? "";
+                    Console.WriteLine($"Using Azure OpenAI {setx.azopenaiEmbeddingDeploymentname}");
+                    kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(setx.azopenaiEmbeddingDeploymentname, setx.azopwnaiEndpoint, setx.azopwnaiApikey);
                     break;
                 case "ollama":
                     // docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
                     Console.WriteLine($"Using Ollama ");
-                    string ollamaEndpoint = configuration["Ollama:Endpoint"] ?? "";
-                    string ollamaEmbeddingModelId = configuration["Ollama:EmbeddingModelId"] ?? "";
-                    kernelBuilder.AddOllamaTextEmbeddingGeneration(ollamaEmbeddingModelId, new Uri(ollamaEndpoint));
+                    //string ollamaEndpoint = configuration["Ollama:Endpoint"] ?? "";
+                    //string ollamaEmbeddingModelId = configuration["Ollama:EmbeddingModelId"] ?? "";
+                    kernelBuilder.AddOllamaTextEmbeddingGeneration(setx.ollamaEmbeddingModelId, new Uri(setx.ollamaEndpoint));
                     break;
                 case "bertonnx":
                     Console.WriteLine("Using Bert embedding");  // download from "git clone https://huggingface.co/TaylorAI/bge-micro-v2"
-                    string bertEmbeddingModelPath = configuration["BertOnnx:EmbeddingModelPath"]!;
-                    string BertEmbeddingVocabPath = configuration["BertOnnx:EmbeddingVocabPath"]!;
-                    kernelBuilder.AddBertOnnxTextEmbeddingGeneration(bertEmbeddingModelPath, BertEmbeddingVocabPath);
+                    //string bertEmbeddingModelPath = configuration["BertOnnx:EmbeddingModelPath"]!;
+                    //string BertEmbeddingVocabPath = configuration["BertOnnx:EmbeddingVocabPath"]!;
+                    kernelBuilder.AddBertOnnxTextEmbeddingGeneration(setx.bertEmbeddingModelPath, setx.bertEmbeddingVocabPath);
                     break;
                 case "huggingface":
                     Console.WriteLine("Using Hugging Face embedding");
-                    string hfEndPoint = configuration["HuggingFace:EndPoint"]!;
-                    string hfApikey = configuration["HuggingFace:Apikey"]!;
-                    kernelBuilder.AddHuggingFaceTextEmbeddingGeneration(new Uri(hfEndPoint), hfApikey);
+                    //string hfEndPoint = configuration["HuggingFace:EndPoint"]!;
+                    //string hfApikey = configuration["HuggingFace:Apikey"]!;
+                    kernelBuilder.AddHuggingFaceTextEmbeddingGeneration(new Uri(setx.hfEndPoint), setx.hfApikey);
                     break;
                 default:
                     Console.WriteLine("Unsupported embedding model");
@@ -70,7 +73,7 @@ namespace DocUploader
                     return;
             }
 
-            switch (vectorStoreType.ToLower())
+            switch (setx.vectorStoreType.ToLower())
             {
                 case "memory":
                     Console.WriteLine("Using In-Memory");
@@ -78,9 +81,9 @@ namespace DocUploader
                     break;
                 case "azaisearch":
                     Console.WriteLine("Using Azure AI Search");
-                    string azaisearchEndpoint = configuration["AzAISearch:Endpoint"] ?? "";
-                    string azaisearchApikey = configuration["AzAISearch:ApiKey"] ?? "";
-                    kernelBuilder.AddAzureAISearchVectorStore(new Uri(azaisearchEndpoint), new AzureKeyCredential(azaisearchApikey));
+                    //string azaisearchEndpoint = configuration["AzAISearch:Endpoint"] ?? "";
+                    //string azaisearchApikey = configuration["AzAISearch:ApiKey"] ?? "";
+                    kernelBuilder.AddAzureAISearchVectorStore(new Uri(setx.azaisearchEndpoint), new AzureKeyCredential(setx.azaisearchApikey));
                     break;
                 case "redis":
                     // docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
@@ -89,9 +92,9 @@ namespace DocUploader
                     break;
                 case "cosmosdb-nosql":
                     Console.WriteLine("Using cosmosdb-nosql");
-                    string cosmosdbConnectionString = configuration["CosmosDB:ConnectionString"] ?? "";
-                    string cosmosdbDatabaseName = configuration["CosmosDB:DatabaseName"] ?? "";
-                    kernelBuilder.AddAzureCosmosDBNoSQLVectorStore(cosmosdbConnectionString, cosmosdbDatabaseName);
+                    //string cosmosdbConnectionString = configuration["CosmosDB:ConnectionString"] ?? "";
+                    //string cosmosdbDatabaseName = configuration["CosmosDB:DatabaseName"] ?? "";
+                    kernelBuilder.AddAzureCosmosDBNoSQLVectorStore(setx.cosmosdbConnectionString, setx.cosmosdbDatabaseName);
                     break;
                 default:
                     Console.WriteLine("Unsupported vector store type");
@@ -101,7 +104,7 @@ namespace DocUploader
 
             kernelBuilder.Services.ConfigureHttpClientDefaults(c => c.AddStandardResilienceHandler());
             kernelBuilder.Services.AddLogging(c => c.AddConsole().SetMinimumLevel(LogLevel.Warning));
-            kernelBuilder.Services.AddSingleton<IConfiguration>(configuration);
+            kernelBuilder.Services.AddSingleton<IConfiguration>(setx.configuration);
             kernelBuilder.Services.AddScoped<DocIntelligence>();
             kernelBuilder.Services.AddScoped<DataUploader>();
             kernelBuilder.Services.AddScoped<DocProcessor>();
@@ -110,7 +113,7 @@ namespace DocUploader
 
             var kernel = kernelBuilder.Build();
 
-            await kernel.Services.GetRequiredService<DocProcessor>().ProcessAndUploadDocumentAsync(dirPath + docName);
+            await kernel.Services.GetRequiredService<DocProcessor>().ProcessAndUploadDocumentAsync(setx.dirPath + setx.docName);
 
 
         }
